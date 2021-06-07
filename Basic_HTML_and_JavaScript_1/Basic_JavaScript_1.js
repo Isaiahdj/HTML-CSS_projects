32840c4e7a0b2970a7ececf316adb0ec71a15ce2
Isaiah <isaiahdj@gmail.com>
2021-06-07 15:34:06 -0700

    Basic Html with Javascript

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,18 @@
+/***************************************************
+ ********** Script For Basic_HTML_1 ****************
+ **************************************************/
+
+/*alert("Hello, World! I am an alert box!");*/
+/* Creats a popup alert in the users browser */
+
+
+/***************************************************
+ ********** Script For Basic_HTML_2 ****************
+ **************************************************/
+
+function My_First_Function() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = 
+    result;
+}
